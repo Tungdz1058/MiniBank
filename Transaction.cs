2042653195fe72e56a086202ab5c0a8ebcc6ddf2
Tungdz1058 <dinhtung11012202@gmail.com>
2025-09-09 22:01:48 +0700

    Add files via upload

---
+++ Transaction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniBank
+{
+    public class Transaction
+    {
+        public string Id { get; }
+        public string AccountNumber { get; }
+        public string Type { get; } // "Deposit", "Withdraw", "Fee", "Interest"
+        public double Amount { get; }
+        public DateTime Timestamp { get; }
+        public string Note { get; }
+
+        public Transaction(string id,string numberaccount,string type,double amount,DateTime timestamp,string note)
+        {
+            this.Id = id;
+            this.AccountNumber = numberaccount;
+            this.Type = type;
+            this.Timestamp = timestamp;
+            this.Amount = amount;
+            this.Note = note;
+        }
+
+        public void inlichsugiaodich()
+        {
+            Console.WriteLine($"ID: {Id}");
+            Console.WriteLine($"Number Account: {AccountNumber}");
+            Console.WriteLine($"Type : {Type}");
+            Console.WriteLine($"Amount : {Amount}");
+            Console.WriteLine($"Time Stamp: {Timestamp}");
+            Console.WriteLine($"Note : {Note}");
+        }
+    }
+}
