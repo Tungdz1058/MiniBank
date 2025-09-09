56d636789730d6568a3e0cb48c3a555c7d1c73a5
Tungdz1058 <dinhtung11012202@gmail.com>
2025-09-09 17:03:49 +0700

    Update BankAccount.cs

--- BankAccount.cs
+++ BankAccount.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,27 +7,37 @@ using MiniBank.Exception;
 
 namespace MiniBank
 {
-    public class BankAccount
+    public class BankAccount 
     {
         public string number_account { get; set; }
         public string own_name { get; set; }
         public double ballance {get; protected set;}
+        private double ball
+        {
+            get { return ballance; }
+            set
+            {
+                if (value < 0) ballance = 0;
+                else ballance = value;
+            }
+        }
         public DateTime created_at { get; set; }
 
         protected BankAccount(string number_account, string own_name, double ballance, DateTime created_at)
         {
+            this.number_account = number_account;
+            this.own_name = own_name;
             try
             {
-                this.number_account = number_account;
-                this.own_name = own_name;
-                this.ballance = ballance;
-                if (ballance < 0) throw new NegativeAmountException("tai khoan khoi tao phai co so tien >=0!!!");
-                this.created_at = created_at;
+                this.ball = ballance;
+                if (ballance < 0) throw new NegativeAmountException("tai khoan khoi tao phai co so tien >=0 he thong tu dat so tien goc = 0!!!");
             }
             catch (NegativeAmountException ex)
             {
                 Console.WriteLine("loi : {0}",ex);
+                Console.WriteLine();
             }
+            this.created_at = created_at;
         }
         public virtual void Deposit(double amount) { } //nap tien
         public virtual void Withdraw(double amount) { } //rut tien
