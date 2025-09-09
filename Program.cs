c93e514f4fb87491f5abd9fa4ed5ed72b3c3f306
Tungdz1058 <dinhtung11012202@gmail.com>
2025-09-09 17:04:31 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,15 +1,21 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MiniBank
-{
+{ 
     public class Program
     {
         static void Main(string[] args)
         {
+            //test 
+            List<BankAccount> minibank = new List<BankAccount>();
+            BankManager a = new BankManager();
+            a.AddNewBankAccount(minibank);
+            a.find_account_number(minibank);
+            a.AddNewBankAccount(minibank);
         }
     }
 }
