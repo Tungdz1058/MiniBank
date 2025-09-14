05e63784810c62ab03c9a8ac764a2f8ccb7a8f71
Tungdz1058 <dinhtung11012202@gmail.com>
2025-09-14 22:35:16 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -15,16 +15,18 @@ namespace MiniBank
             List<Transaction> minitransaction = new List<Transaction>();
             BankManager a = new BankManager();
             a.AddNewBankAccount(minibank);
-            a.AddNewBankAccount(minibank);
-            a.AddNewBankAccount(minibank);
+            //a.AddNewBankAccount(minibank);
+            //a.AddNewBankAccount(minibank);
             a.monthlyinterest(minitransaction, minibank, 0.01, 10);
-            foreach(var b in minitransaction)
-            {
-                b.inlichsugiaodich();
-                Console.WriteLine();
-            }
-            Console.WriteLine();
-            a.find_account_number(minibank);
+            //foreach(var b in minitransaction)
+            //{
+            //    b.inlichsugiaodich();
+            //    Console.WriteLine();
+            //}
+
+            //Console.WriteLine();
+            //a.find_account_number(minibank);
+            a.thongke(minibank);
         }
     }
 }
